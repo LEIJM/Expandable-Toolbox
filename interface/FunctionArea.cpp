51187462fb4745ab067d253755a8a9f584140da2
LEIJM <123279988+LEIJM@users.noreply.github.com>
2024-10-16 21:30:32 +0800

    Add files via upload

--- interface/FunctionArea.cpp
+++ interface/FunctionArea.cpp
@@ -5,6 +5,7 @@
 #include "functionarea.h"
 #include <QListWidgetItem>
 #include <QDirIterator>
+#include <qthread.h>
 
 FunctionArea::FunctionArea(QWidget *parent)
         : QWidget(parent) {
@@ -23,42 +24,52 @@ void FunctionArea::showShortcuts(const QString &folderName) {
     // 清空当前快捷方式列表
     shortcuts->clear();
 
-    // 读取指定文件夹及其子文件夹下的 exe 文件快捷方式
+    // 读取指定文件夹及其子文件夹下的 exe 和 cmd 文件
     QDir toolsDir("tools/" + folderName);  // 确认此路径是否正确
 
     // 创建QDirIterator来递归遍历文件夹
-    QDirIterator dirIterator(toolsDir.path(), QDir::Files | QDir::NoSymLinks, QDirIterator::Subdirectories);
+    QDirIterator dirIterator(toolsDir.path(), QStringList() << "*.exe" << "*.cmd", QDir::Files | QDir::NoSymLinks, QDirIterator::Subdirectories);
     while (dirIterator.hasNext()) {
         dirIterator.next();
-        QString filePath = dirIterator.filePath();
         QFileInfo fileInfo = dirIterator.fileInfo();
 
-        // 检查文件是否以.exe结尾
-        if (fileInfo.isFile() && fileInfo.suffix() == "exe") {
+        // 检查文件是否以 .exe 或 .cmd 结尾
+        if (fileInfo.isFile() && (fileInfo.suffix().compare("exe", Qt::CaseInsensitive) == 0 || fileInfo.suffix().compare("cmd", Qt::CaseInsensitive) == 0)) {
             QListWidgetItem *item = new QListWidgetItem(fileInfo.completeBaseName(), shortcuts);
-            item->setData(Qt::UserRole, filePath);
+            item->setData(Qt::UserRole, fileInfo.absoluteFilePath());
         }
     }
 }
 
-
 void FunctionArea::onShortcutDoubleClicked(QListWidgetItem *item) {
-    if (item) {
-        QString exeFilePath = item->data(Qt::UserRole).toString();
+    if (!item) {
+        return;
+    }
 
-        // 检查文件是否存在
-        if (!QFile::exists(exeFilePath)) {
-            return;
-        }
+    QString exeFilePath = item->data(Qt::UserRole).toString();
 
-        // 尝试启动程序
-        QProcess process;
-        process.start(exeFilePath);
+    // 检查文件是否存在
+    if (!QFile::exists(exeFilePath)) {
+        qDebug() << "文件不存在: " << exeFilePath;
+        return;
+    }
 
-        // 等待程序启动
-        if (process.waitForStarted()) {
-            // 等待程序结束
-            process.waitForFinished();
-        }
+    // 创建新进程对象，使用智能指针自动管理资源
+    QSharedPointer<QProcess> process(new QProcess);
+
+    // 设置进程参数
+    process->setProcessChannelMode(QProcess::MergedChannels);
+    process->setProgram(exeFilePath);
+
+    // 为避免阻塞GUI，连接结束信号来安全地处理进程和线程的清理
+    connect(process.get(), QOverload<int, QProcess::ExitStatus>::of(&QProcess::finished),
+            this, [process](int exitCode, QProcess::ExitStatus exitStatus) {
+                qDebug() << "Process exited with code " << exitCode << " and status " << exitStatus;
+            });
+
+    // 在不阻塞GUI的情况下启动进程
+    process->start();
+    if (!process->waitForStarted()) {
+        qDebug() << "无法启动进程: " << exeFilePath;
     }
 }
